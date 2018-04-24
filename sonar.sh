5d40ffd89618657e96deceb70d9b0df35a81a180
antonio.dieppa <antonio.dieppa@gamesys.co.uk>
2018-04-24 12:36:15 +0100

    test

--- sonar.sh
+++ sonar.sh
@@ -2,13 +2,15 @@
 #$1 -> PR number
 
 # It uses token from Github that only has access to comment and update the PR's status
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar \
+mvn sonar:sonar \
         -Psonarqube \
         -Dmaven.test.failure.ignore=true \
         -Dsonar.analysis.mode=preview \
-        -Dsonar.github.pullRequest=$1 \
+        -Dsonar.sources=src/main \
+        -Dsonar.tests=src/test \
+        -Dsonar.github.pullRequest=5 \
         -Dsonar.github.repository=cloudyrock/mongock \
-        -Dsonar.github.oauth=7416a97904ca7f19ffbebf36338f2a171888d41c \
+        -Dsonar.github.oauth=9c7dd30e840e1f24f50a2cf283f63e954d6b7f4f \
         -Dsonar.host.url=https://sonarcloud.io \
         -Dsonar.organization=dieppa-github \
         -Dsonar.login=b399a4341c829aa96ec3a1c164ca1db1dc0e8e0d
