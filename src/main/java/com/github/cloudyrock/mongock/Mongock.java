c3aa28edd4508cd0f47f8ea5ff7852e0126c707b
antonio.dieppa <antonio.dieppa@gamesys.co.uk>
2018-04-24 13:26:02 +0100

    added broken double checked locking

--- src/main/java/com/github/cloudyrock/mongock/Mongock.java
+++ src/main/java/com/github/cloudyrock/mongock/Mongock.java
@@ -91,6 +91,8 @@ public class Mongock implements InitializingBean, Closeable {
       return;
     }
 
+    Object o = getObj();
+
     try {
       lockChecker.acquireLockDefault();
       executeMigration();
@@ -210,5 +212,15 @@ public class Mongock implements InitializingBean, Closeable {
     }
   }
 
-
+  Object obj;
+  public Object getObj() {
+    if (obj == null) {
+      synchronized (obj) {
+        if (obj == null) {
+          obj = new Object();
+        }
+      }
+    }
+    return obj;
+  }
 }
