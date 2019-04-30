23a8260a1e8ab2079954f7bd86ad84df7490c63e
Nathaniel Brown <49286796+C4X2@users.noreply.github.com>
2019-04-30 19:48:55 -0400

    Update StringUtil.java
    The built-in method isBlank allows us to make our code shorter and more readable.

--- mongock-core/src/main/java/com/github/cloudyrock/mongock/StringUtils.java
+++ mongock-core/src/main/java/com/github/cloudyrock/mongock/StringUtils.java
@@ -2,16 +2,11 @@ package com.github.cloudyrock.mongock;
 
 public class StringUtils {
 
-  public static boolean hasText(String str) {
-    if (str == null || str.isEmpty()) {
-      return false;
-    }
-    int strLen = str.length();
-    for (int i = 0; i < strLen; i++) {
-      if (!Character.isWhitespace(str.charAt(i))) {
-        return true;
-      }
-    }
-    return false;
-  }
+	  public static boolean hasText(String str) {
+		    if (str == null || str.isEmpty() || str.isBlank()) {
+		      return false;
+		    }
+
+		    return true;
+		  }
 }
