8b5b487e3d91c0b5734ff196adea8d66f92feef9
dieppa <aperezdieppa@gmail.com>
2020-02-17 09:06:42 +0000

    [ISSUE-133] - wip recreating successfully the required uniqueKey

--- mongock-core/src/main/java/com/github/cloudyrock/mongock/MongoRepositoryBase.java
+++ mongock-core/src/main/java/com/github/cloudyrock/mongock/MongoRepositoryBase.java
@@ -8,6 +8,10 @@ import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 
 abstract class MongoRepositoryBase implements Repository{
 
@@ -19,35 +23,90 @@ abstract class MongoRepositoryBase implements Repository{
 
   MongoRepositoryBase(MongoDatabase mongoDatabase, String collectionName, String[] uniqueFields) {
     this.collection = mongoDatabase.getCollection(collectionName);
-    this.fullCollectionName =
-        collection.getNamespace().getDatabaseName() + "." + collection.getNamespace().getCollectionName();
+    this.fullCollectionName = collection.getNamespace().getDatabaseName() + "." + collection.getNamespace().getCollectionName();
     this.uniqueFields = uniqueFields;
   }
 
   public synchronized void initialize() {
     if (!this.ensuredCollectionIndex) {
-      final Document index = findRequiredUniqueIndex();
-      if (index == null) {
+      cleanResidualUniqueKeys();
+      if(isIndexCreationRequired()) {
         createRequiredUniqueIndex();
-        logger.debug("Index in collection {} was created", getCollectionName());
-      } else if (!isUniqueIndex(index)) {
-        dropIndex(index);
-        createRequiredUniqueIndex();
-        logger.debug("Index in collection {} was recreated", getCollectionName());
       }
       this.ensuredCollectionIndex = true;
     }
+  }
 
+//  public synchronized void initialize() {
+//    if (!this.ensuredCollectionIndex) {
+//      final Document index = findRequiredUniqueIndex();
+//      if (index == null) {
+//        createRequiredUniqueIndex();
+//        logger.debug("Index in collection {} was created", getCollectionName());
+//      } else if (!isRightIndex(index)) {
+//        dropIndex(index);
+//        createRequiredUniqueIndex();
+//        logger.debug("Index in collection {} was recreated", getCollectionName());
+//      }
+//      this.ensuredCollectionIndex = true;
+//    }
+//  }
+
+
+  private void cleanResidualUniqueKeys() {
+    logger.debug("Removing residual uniqueKeys for collection [{}]", getCollectionName());
+    final ListIndexesIterable<Document> indexes = collection.listIndexes();
+    for (Document index : indexes) {
+      if (needsToBeRemoved(index)) {
+        dropIndex(index);
+        logger.debug("Removed residual uniqueKey [{}] for collection [{}]", index.toString(), getCollectionName());
+      } else {
+        logger.debug("Keeping right uniqueKey [{}] for collection [{}]", index.toString(), getCollectionName());
+      }
+    }
+  }
+
+  private boolean needsToBeRemoved(Document index) {
+    return isNotIdIndex(index) && isUniqueIndex(index) && !isRightIndex(index);
+  }
+
+  private boolean isNotIdIndex(Document index) {
+    return !(((Document) index.get("key")).getInteger("_id", 0) == 1);
+
+  }
+
+  private boolean isIndexCreationRequired() {
+    return StreamSupport.stream(collection.listIndexes().spliterator(), false)
+        .noneMatch(this::isRightIndex);
   }
 
   void createRequiredUniqueIndex() {
     collection.createIndex(getIndexDocument(uniqueFields), new IndexOptions().unique(true));
+    logger.debug("Index in collection {} was recreated", getCollectionName());
   }
+
+  //todo what if I require 2 fields, but the current key contains those two plus more?? -> that wouldn't be right
+  boolean isRightIndex(Document index) {
+    final Document key = (Document) index.get("key");
+    Set<String> keySet = key.keySet();
+    for (String uniqueField : uniqueFields) {
+      if (key.getInteger(uniqueField, 0) != 1) {
+        return false;
+      }
+    }
+    return isUniqueIndex(index);
+  }
+
+  private boolean isUniqueIndex(Document index) {
+    return fullCollectionName.equals(index.getString("ns")) && index.getBoolean("unique", false);
+  }
+
+
 
   Document findRequiredUniqueIndex() {
     final ListIndexesIterable<Document> indexes = collection.listIndexes();
     for (Document index : indexes) {
-      if (isUniqueIndex(index)) {
+      if (isRightIndex(index)) {
         return index;
       }
     }
@@ -66,15 +125,7 @@ abstract class MongoRepositoryBase implements Repository{
     return indexDocument;
   }
 
-  boolean isUniqueIndex(Document index) {
-    final Document key = (Document) index.get("key");
-    for (String uniqueField : uniqueFields) {
-      if (key.getInteger(uniqueField, 0) != 1) {
-        return false;
-      }
-    }
-    return fullCollectionName.equals(index.getString("ns")) && index.getBoolean("unique", false);
-  }
+
 
   void dropIndex(Document index) {
     collection.dropIndex(index.get("name").toString());
