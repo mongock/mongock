9a72ad1aa9e7e1b7f172c3e7284540babacc5c12
dieppa <aperezdieppa@gmail.com>
2020-03-30 21:52:33 +0100

    v3_issue_139_re_setCollectLockCollectionName] fixed tests

--- mongock-core/src/test/java/com/github/cloudyrock/mongock/ChangeEntryRepositoryTest.java
+++ mongock-core/src/test/java/com/github/cloudyrock/mongock/ChangeEntryRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
@@ -34,18 +35,21 @@ public class ChangeEntryRepositoryTest extends IndependentDbIntegrationTestBase 
   }
 
   @Test
-  public void shouldNoCreateUniqueIndex_whenEnsureIndex_IfAlreadyCreated() throws MongockException {
+  public void shouldNotCreateUniqueIndex_whenEnsureIndex_IfAlreadyCreated() throws MongockException {
 
     // when
-    ChangeEntryMongoRepository dao = mock(ChangeEntryMongoRepository.class);
-    when(dao.isRightIndex(any(Document.class))).thenReturn(true);
+
+    ChangeEntryMongoRepository dao = spy(new ChangeEntryMongoRepository(CHANGELOG_COLLECTION_NAME, db));
     doCallRealMethod().when(dao).initialize();
 
     // when
     dao.initialize();
+    dao.initialize();
 
     //then
-    verify(dao, times(0)).createRequiredUniqueIndex();
+    verify(dao, times(1)).createRequiredUniqueIndex();//it's only called in the first initialized, as
+    //the keys are not created yet. In the second initialize call, keys are already in place, so createRequiredUniqueIndex won't bbe called
+
     // and not
     verify(dao, times(0)).dropIndex(new Document());
   }
