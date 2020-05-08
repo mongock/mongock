97ddee463beed654099364e8f177e7827fd7849b
dieppa <aperezdieppa@gmail.com>
2020-05-08 20:27:43 +0100

    added all drivers

---
+++ drivers/mongodb-springdata-v3-driver/src/main/java/com/github/cloudyrock/mongock/driver/mongodb/springdata/v3/decorator/operation/executable/aggregation/TerminatingAggregationDecorator.java
@@ -0,0 +1,22 @@
+package com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.operation.executable.aggregation;
+
+import io.changock.driver.api.lock.guard.decorator.Invokable;
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.CloseableIteratorDecoratorImpl;
+import org.springframework.data.mongodb.core.ExecutableAggregationOperation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.util.CloseableIterator;
+
+public interface TerminatingAggregationDecorator<T> extends Invokable, ExecutableAggregationOperation.TerminatingAggregation<T> {
+
+  ExecutableAggregationOperation.TerminatingAggregation<T> getImpl();
+
+  @Override
+  default AggregationResults<T> all() {
+    return getInvoker().invoke(()-> getImpl().all());
+  }
+
+  @Override
+  default CloseableIterator<T> stream() {
+    return new CloseableIteratorDecoratorImpl<>(getInvoker().invoke(()-> getImpl().stream()), getInvoker());
+  }
+}
---
+++ drivers/mongodb-springdata-v3-driver/src/main/java/com/github/cloudyrock/mongock/driver/mongodb/springdata/v3/decorator/operation/executable/update/impl/TerminatingFindAndModifyDecoratorImpl.java
@@ -0,0 +1,14 @@
+package com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.operation.executable.update.impl;
+
+import io.changock.driver.api.lock.guard.decorator.DecoratorBase;
+import io.changock.driver.api.lock.guard.invoker.LockGuardInvoker;
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.operation.executable.update.TerminatingFindAndModifyDecorator;
+import org.springframework.data.mongodb.core.ExecutableUpdateOperation;
+
+public class TerminatingFindAndModifyDecoratorImpl<T> extends DecoratorBase<ExecutableUpdateOperation.TerminatingFindAndModify<T>> implements TerminatingFindAndModifyDecorator<T> {
+
+  public TerminatingFindAndModifyDecoratorImpl(ExecutableUpdateOperation.TerminatingFindAndModify<T> impl, LockGuardInvoker invoker) {
+    super(impl, invoker);
+  }
+
+}
