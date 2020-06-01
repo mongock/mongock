c6a7e22059365faab7409332a25a25dded2c3f2a
dieppa <aperezdieppa@gmail.com>
2020-06-01 08:41:45 +0100

    wip springdata2

---
+++ drivers/mongodb-springdata-v2-driver/src/test/java/com/github/cloudyrock/mongock/driver/mongodb/springdata/v2/driver/SpringDataMongo2DriverITest.java
@@ -0,0 +1,26 @@
+package com.github.cloudyrock.mongock.driver.mongodb.springdata.v2.driver;
+
+
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v2.SpringDataMongo2Driver;
+import com.github.cloudyrock.mongock.driver.mongodb.test.template.MongoDriverITestBase;
+import com.github.cloudyrock.mongock.driver.mongodb.test.template.util.MongoDbDriverTestAdapter;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+public class SpringDataMongo2DriverITest extends MongoDriverITestBase {
+
+  @Override
+  protected SpringDataMongo2Driver getDriver() {
+    SpringDataMongo2Driver driver = new SpringDataMongo2Driver(getMongoTemplate());
+    driver.setChangeLogCollectionName(CHANGELOG_COLLECTION_NAME);
+    return driver;
+  }
+
+  @Override
+  protected MongoDbDriverTestAdapter getAdapter(String collectionName) {
+    return new SpringData2DriverTestAdapterImpl(getDataBase().getCollection(collectionName));
+  }
+
+  private MongoTemplate getMongoTemplate() {
+    return new MongoTemplate(this.getMongoClient(), this.getDataBase().getName());
+  }
+}
