ef06e36b38e79fe1fcb0600e22de9e6a7caefc53
dieppa <aperezdieppa@gmail.com>
2020-07-02 08:16:46 +0100

    v4 refactor legacy

--- mongock-api/src/main/java/com/github/cloudyrock/mongock/migration/MongockLegacyMigration.java
+++ mongock-api/src/main/java/com/github/cloudyrock/mongock/migration/MongockLegacyMigration.java
@@ -1,6 +1,7 @@
 package com.github.cloudyrock.mongock.migration;
 
 import io.changock.runner.core.builder.configuration.LegacyMigration;
+import io.changock.runner.core.builder.configuration.LegacyMigrationMappingFields;
 
 public class MongockLegacyMigration extends LegacyMigration {
 
@@ -13,6 +14,33 @@ public class MongockLegacyMigration extends LegacyMigration {
 
   public MongockLegacyMigration(String collectionName) {
     this.collectionName = collectionName;
+  }
+
+  public MongockLegacyMigration(String collectionName,
+                                boolean failFast,
+                                String changeId,
+                                String author,
+                                String timestamp,
+                                String changeLogClass,
+                                String changeSetMethod) {
+
+    this(collectionName, failFast, changeId, author, timestamp, changeLogClass, changeSetMethod, null, null);
+    this.failFast = failFast;
+  }
+
+  public MongockLegacyMigration(String collectionName,
+                                boolean failFast,
+                                String changeId,
+                                String author,
+                                String timestamp,
+                                String changeLogClass,
+                                String changeSetMethod,
+                                String metadata,
+                                Integer changesCountExpectation) {
+    this.collectionName = collectionName;
+    this.failFast = failFast;
+    this.setChangesCountExpectation(changesCountExpectation);
+    this.setMappingFields(new LegacyMigrationMappingFields(changeId, author, timestamp, changeLogClass, changeSetMethod, metadata));
   }
 
   public String getCollectionName() {
