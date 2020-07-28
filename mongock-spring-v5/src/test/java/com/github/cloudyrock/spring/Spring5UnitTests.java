234218f8fdb642cc07c2245544ea58f714d95c03
dieppa <aperezdieppa@gmail.com>
2020-07-28 17:02:36 +0100

    custom beans + renamed properties namespace from 'spring.mongock' to just 'mongock'

--- mongock-spring-v5/src/test/java/com/github/cloudyrock/spring/Spring5UnitTests.java
+++
@@ -1,9 +0,0 @@
-package com.github.cloudyrock.spring;
-
-
-//TODO Add here relevant unit tests
-// integration tests are in module/project  mongock-spring-v5-integration-tests
-public class Spring5UnitTests {
-
-
-}
