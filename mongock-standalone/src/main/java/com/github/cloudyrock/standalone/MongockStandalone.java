c8d611c8b4b56177aec55e3754aa5d08bd4687cb
dieppa <aperezdieppa@gmail.com>
2020-11-27 17:42:16 +0000

    refactor

--- mongock-standalone/src/main/java/com/github/cloudyrock/standalone/MongockStandalone.java
+++ mongock-standalone/src/main/java/com/github/cloudyrock/standalone/MongockStandalone.java
@@ -1,30 +1,34 @@
 package com.github.cloudyrock.standalone;
 
 import com.github.cloudyrock.mongock.MongockAnnotationProcessor;
-import com.github.cloudyrock.mongock.MongockConnectionDriver;
-
+import io.changock.driver.api.driver.ConnectionDriver;
 import io.changock.migration.api.config.ChangockConfiguration;
 import io.changock.runner.core.builder.DriverBuilderConfigurable;
 import io.changock.runner.core.event.EventPublisher;
 import io.changock.runner.core.executor.ChangeLogService;
 import io.changock.runner.core.executor.MigrationExecutor;
-import io.changock.runner.standalone.StandaloneBuilder;
+import io.changock.runner.standalone.StandaloneBuilderBase;
 import io.changock.runner.standalone.StandaloneRunner;
 
 /**
  * Mongock runner
  *
+ * Deprecated. Use ChangockStandalone instead
+ *
+ * @see io.changock.runner.standalone.ChangockStandalone
+ *
  * @since 26/07/2020
  */
+@Deprecated
 public class MongockStandalone {
 
 
-  public static DriverBuilderConfigurable<Builder, MongockConnectionDriver, ChangockConfiguration> builder() {
+  public static DriverBuilderConfigurable<Builder, ConnectionDriver, ChangockConfiguration> builder() {
     return new Builder();
   }
 
 
-  public static class Builder extends StandaloneBuilder<Builder, MongockConnectionDriver> {
+  public static class Builder extends StandaloneBuilderBase<Builder, ConnectionDriver> {
 
     private Builder() {
       this.overrideAnnoatationProcessor(new MongockAnnotationProcessor());
