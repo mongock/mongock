64623a76ac1174ca2e31401532c02f6392369a54
Dieppa on the rocks <aperezdieppa@gmail.com>
2021-03-03 14:25:35 +0000

    wip (#304)

--- mongodb-springdata-v2-driver/src/main/java/com/github/cloudyrock/mongock/driver/mongodb/springdata/v2/config/SpringDataMongoV2Context.java
+++ mongodb-springdata-v2-driver/src/main/java/com/github/cloudyrock/mongock/driver/mongodb/springdata/v2/config/SpringDataMongoV2Context.java
@@ -1,35 +1,38 @@
-package com.github.cloudyrock.mongock.driver.mongodb.springdata.v3;
+package com.github.cloudyrock.mongock.driver.mongodb.springdata.v2.config;
 
 import com.github.cloudyrock.mongock.config.MongockConfiguration;
 import com.github.cloudyrock.mongock.driver.api.driver.ConnectionDriver;
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v2.SpringDataMongoV2Driver;
 import com.mongodb.ReadConcern;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.Optional;
 
 @Configuration
+@Import(MongoDBConfiguration.class)
 @ConditionalOnExpression("${mongock.enabled:true}")
-public class SpringDataMongoV3Context {
+public class SpringDataMongoV2Context {
 
   @Bean
   public ConnectionDriver connectionDriver(MongoTemplate mongoTemplate,
                                            MongockConfiguration config,
                                            MongoDBConfiguration mongoDbConfig,
                                            Optional<MongoTransactionManager> txManagerOpt) {
-    SpringDataMongoV3Driver driver = buildDriver(mongoTemplate, config, mongoDbConfig, txManagerOpt);
+    SpringDataMongoV2Driver driver = buildDriver(mongoTemplate, config, mongoDbConfig, txManagerOpt);
     driver.initialize();
     return driver;
   }
 
-  private SpringDataMongoV3Driver buildDriver(MongoTemplate mongoTemplate,
+  private SpringDataMongoV2Driver buildDriver(MongoTemplate mongoTemplate,
                                               MongockConfiguration config,
                                               MongoDBConfiguration mongoDbConfig,
                                               Optional<MongoTransactionManager> txManagerOpt) {
-    SpringDataMongoV3Driver driver = SpringDataMongoV3Driver.withLockSetting(
+    SpringDataMongoV2Driver driver = SpringDataMongoV2Driver.withLockSetting(
         mongoTemplate,
         config.getLockAcquiredForMinutes(),
         config.getMaxWaitingForLockMinutes(),
@@ -42,14 +45,14 @@ public class SpringDataMongoV3Context {
 
   private void setGenericDriverConfig(MongockConfiguration config,
                                       Optional<MongoTransactionManager> txManagerOpt,
-                                      SpringDataMongoV3Driver driver) {
+                                      SpringDataMongoV2Driver driver) {
     setTransactionManager(config, txManagerOpt, driver);
     driver.setChangeLogRepositoryName(config.getChangeLogRepositoryName());
     driver.setLockRepositoryName(config.getLockRepositoryName());
     driver.setIndexCreation(config.isIndexCreation());
   }
 
-  private void setMongoDBConfig(MongoDBConfiguration mongoDbConfig, SpringDataMongoV3Driver driver) {
+  private void setMongoDBConfig(MongoDBConfiguration mongoDbConfig, SpringDataMongoV2Driver driver) {
     driver.setWriteConcern(mongoDbConfig.getBuiltMongoDBWriteConcern());
     driver.setReadConcern(new ReadConcern(mongoDbConfig.getReadConcern()));
     driver.setReadPreference(mongoDbConfig.getReadPreference().getValue());
@@ -59,18 +62,16 @@ public class SpringDataMongoV3Context {
 
   private void setTransactionManager(MongockConfiguration config,
                                      Optional<MongoTransactionManager> txManagerOpt,
-                                     SpringDataMongoV3Driver driver) {
+                                     SpringDataMongoV2Driver driver) {
     txManagerOpt
         .filter(tx-> config.isTransactionEnabled())
         .map(tx-> {
           driver.enableTransactionWithTxManager(tx);
           return true;
         }).orElseGet(()-> {
-          driver.disableTransaction();
-          return false;
-        });
+      driver.disableTransaction();
+      return false;
+    });
 
   }
-
-
 }
