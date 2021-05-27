8eb7937bff3950c1b30db1242c991eb09990fdc9
dieppa <aperezdieppa@gmail.com>
2021-05-27 21:59:54 +0100

    fix compilation tests

--- integration-tests/src/test/java/com/github/cloudyrock/mongock/integrationtests/spring5/springdata3/ApplicationRunnerTestBase.java
+++ integration-tests/src/test/java/com/github/cloudyrock/mongock/integrationtests/spring5/springdata3/ApplicationRunnerTestBase.java
@@ -3,8 +3,8 @@ package com.github.cloudyrock.mongock.integrationtests.spring5.springdata3;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongoV3Driver;
 import com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.util.Constants;
 import com.github.cloudyrock.mongock.integrationtests.spring5.springdata3.util.MongoContainer;
+import com.github.cloudyrock.springboot.MigrationSpringbootBuilder;
 import com.github.cloudyrock.springboot.MongockSpringboot;
-import com.github.cloudyrock.springboot.base.builder.migration.MigrationSpringbootBuilder;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.mockito.Mockito;
@@ -14,31 +14,31 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 
 abstract class ApplicationRunnerTestBase {
 
-    protected MongoClient mongoClient;
-    protected MongoTemplate mongoTemplate;
+  protected MongoClient mongoClient;
+  protected MongoTemplate mongoTemplate;
 
-    protected void start(String mongoVersion) {
-        MongoContainer mongoDBContainer = RuntimeTestUtil.startMongoDbContainer(mongoVersion);
-        mongoClient = MongoClients.create(mongoDBContainer.getReplicaSetUrl());
-        mongoTemplate = new MongoTemplate(mongoClient, RuntimeTestUtil.DEFAULT_DATABASE_NAME);
-    }
+  protected void start(String mongoVersion) {
+    MongoContainer mongoDBContainer = RuntimeTestUtil.startMongoDbContainer(mongoVersion);
+    mongoClient = MongoClients.create(mongoDBContainer.getReplicaSetUrl());
+    mongoTemplate = new MongoTemplate(mongoClient, RuntimeTestUtil.DEFAULT_DATABASE_NAME);
+  }
 
-    protected SpringDataMongoV3Driver buildDriver() {
-        SpringDataMongoV3Driver driver = SpringDataMongoV3Driver.withDefaultLock(mongoTemplate);
-        driver.setChangeLogRepositoryName(Constants.CHANGELOG_COLLECTION_NAME);
-        return driver;
-    }
+  protected SpringDataMongoV3Driver buildDriver() {
+    SpringDataMongoV3Driver driver = SpringDataMongoV3Driver.withDefaultLock(mongoTemplate);
+    driver.setChangeLogRepositoryName(Constants.CHANGELOG_COLLECTION_NAME);
+    return driver;
+  }
 
-    protected MigrationSpringbootBuilder getBasicBuilder(String packagePath) {
-        return MongockSpringboot.builder()
-                .setDriver(buildDriver())
-                .addChangeLogsScanPackage(packagePath)
-                .setSpringContext(getApplicationContext());
-    }
+  protected MigrationSpringbootBuilder getBasicBuilder(String packagePath) {
+    return MongockSpringboot.builder()
+        .setDriver(buildDriver())
+        .addChangeLogsScanPackage(packagePath)
+        .setSpringContext(getApplicationContext());
+  }
 
-    protected ApplicationContext getApplicationContext() {
-        ApplicationContext context = Mockito.mock(ApplicationContext.class);
-        Mockito.when(context.getBean(Environment.class)).thenReturn(Mockito.mock(Environment.class));
-        return context;
-    }
+  protected ApplicationContext getApplicationContext() {
+    ApplicationContext context = Mockito.mock(ApplicationContext.class);
+    Mockito.when(context.getBean(Environment.class)).thenReturn(Mockito.mock(Environment.class));
+    return context;
+  }
 }
