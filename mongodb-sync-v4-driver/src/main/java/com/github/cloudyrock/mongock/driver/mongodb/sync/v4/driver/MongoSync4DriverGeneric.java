3d7f1d00f0f00741a754c6016456a5c2da7ca59e
Oliver Santana <oliver.sm@gmail.com>
2021-06-03 20:28:18 +0100

    Generalized MongoSync4Driver to share specific code in inherited classes.

---
+++ mongodb-sync-v4-driver/src/main/java/com/github/cloudyrock/mongock/driver/mongodb/sync/v4/driver/MongoSync4DriverGeneric.java
@@ -0,0 +1,62 @@
+package com.github.cloudyrock.mongock.driver.mongodb.sync.v4.driver;
+
+import com.github.cloudyrock.mongock.driver.api.driver.Transactioner;
+import com.github.cloudyrock.mongock.driver.api.entry.ChangeEntry;
+import com.github.cloudyrock.mongock.exception.MongockException;
+import com.github.cloudyrock.mongock.utils.annotation.NotThreadSafe;
+import com.mongodb.MongoClientException;
+import com.mongodb.client.ClientSession;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.TransactionBody;
+
+import java.util.Optional;
+
+@NotThreadSafe
+public abstract class MongoSync4DriverGeneric<CHANGE_ENTRY extends ChangeEntry> extends MongoSync4DriverBase<CHANGE_ENTRY> {
+
+  private final MongoClient mongoClient;
+  private boolean transactionEnabled = true;
+
+  protected MongoSync4DriverGeneric(MongoClient mongoClient,
+                             String databaseName,
+                             long lockAcquiredForMillis,
+                             long lockQuitTryingAfterMillis,
+                             long lockTryFrequencyMillis) {
+    super(mongoClient.getDatabase(databaseName), lockAcquiredForMillis, lockQuitTryingAfterMillis, lockTryFrequencyMillis);
+    this.mongoClient = mongoClient;
+  }
+
+  @Override
+  public void executeInTransaction(Runnable operation) {
+    ClientSession clientSession;
+    try {
+      clientSession = mongoClient.startSession();
+    } catch (MongoClientException ex) {
+      throw new MongockException("ERROR starting session. If Mongock is connected to a MongoDB cluster which doesn't support transactions, you must to disable transactions", ex);
+    }
+    try {
+      clientSession.withTransaction(getTransactionBody(operation), txOptions);
+    } catch (Exception ex) {
+      throw new MongockException(ex);
+    } finally {
+      clientSession.close();
+    }
+  }
+
+  private TransactionBody<String> getTransactionBody(Runnable operation) {
+    return () -> {
+      operation.run();
+      return "Mongock transaction operation";
+    };
+  }
+
+  @Override
+  public void disableTransaction() {
+    transactionEnabled = false;
+  }
+
+  @Override
+  public Optional<Transactioner> getTransactioner() {
+    return Optional.ofNullable(transactionEnabled ? this : null);
+  }
+}
