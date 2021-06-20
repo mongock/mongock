026dae0dda4dff8d35011e58dded4100f38ba222
dieppa <aperezdieppa@gmail.com>
2021-06-20 18:46:07 +0100

    saveOrUpdate

--- mongodb-springdata-v3-driver/src/main/java/com/github/cloudyrock/mongock/driver/mongodb/springdata/v3/SpringDataMongoV3ChangeEntryRepository.java
+++ mongodb-springdata-v3-driver/src/main/java/com/github/cloudyrock/mongock/driver/mongodb/springdata/v3/SpringDataMongoV3ChangeEntryRepository.java
@@ -5,8 +5,13 @@ import com.github.cloudyrock.mongock.driver.core.entry.ChangeEntryRepository;
 import com.github.cloudyrock.mongock.driver.mongodb.sync.v4.repository.MongoSync4ChangeEntryRepository;
 import com.github.cloudyrock.mongock.driver.mongodb.sync.v4.repository.ReadWriteConfiguration;
 import com.github.cloudyrock.mongock.exception.MongockException;
+import com.mongodb.client.model.UpdateOptions;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 public class SpringDataMongoV3ChangeEntryRepository<CHANGE_ENTRY extends ChangeEntry> extends MongoSync4ChangeEntryRepository<CHANGE_ENTRY> implements ChangeEntryRepository<CHANGE_ENTRY, Document> {
 
@@ -29,5 +34,26 @@ public class SpringDataMongoV3ChangeEntryRepository<CHANGE_ENTRY extends ChangeE
   public void save(CHANGE_ENTRY changeEntry) throws MongockException {
     mongoTemplate.save(changeEntry, collection.getNamespace().getCollectionName());
   }
+
+  @Override
+  public void saveOrUpdate(CHANGE_ENTRY changeEntry) throws MongockException {
+
+    Query filter = new Query().addCriteria(new Criteria()
+        .andOperator(
+            Criteria.where(KEY_EXECUTION_ID).is(changeEntry.getExecutionId()),
+            Criteria.where(KEY_CHANGE_ID).is(changeEntry.getChangeId()),
+            Criteria.where(KEY_AUTHOR).is(changeEntry.getAuthor())));
+    mongoTemplate.upsert(filter, getUpdateFromEntity(changeEntry), collection.getNamespace().getCollectionName());
+  }
+
+  private Update getUpdateFromEntity(CHANGE_ENTRY changeEntry) {
+    Update updateChangeEntry = new Update();
+    Document entityDocu = toEntity(changeEntry);
+    entityDocu.forEach(updateChangeEntry::set);
+    return updateChangeEntry;
+  }
+
+
+
 
 }
