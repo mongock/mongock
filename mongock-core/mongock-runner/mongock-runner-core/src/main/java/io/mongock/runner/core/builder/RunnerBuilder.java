142cd36a8c45235430a59cf03e3ad103d5c66c5f
dieppa <aperezdieppa@gmail.com>
2021-10-06 11:44:32 +0100

    wip

---
+++ mongock-core/mongock-runner/mongock-runner-core/src/main/java/io/mongock/runner/core/builder/RunnerBuilder.java
@@ -0,0 +1,34 @@
+package io.mongock.runner.core.builder;
+
+import io.mongock.api.config.MongockConfiguration;
+import io.mongock.driver.api.entry.ChangeEntry;
+import io.mongock.runner.core.builder.roles.ChangeLogScanner;
+import io.mongock.runner.core.builder.roles.MigrationWriter;
+import io.mongock.runner.core.builder.roles.DependencyInjectable;
+import io.mongock.runner.core.builder.roles.DriverConnectable;
+import io.mongock.runner.core.builder.roles.LegacyMigrator;
+import io.mongock.runner.core.builder.roles.MongockRunnable;
+import io.mongock.runner.core.builder.roles.ServiceIdentificable;
+import io.mongock.runner.core.builder.roles.SystemVersionable;
+import io.mongock.runner.core.builder.roles.TransactionStrategiable;
+import io.mongock.runner.core.builder.roles.Transactionable;
+import io.mongock.runner.core.builder.roles.Typeable;
+
+@SuppressWarnings("all")
+public interface RunnerBuilder<
+    SELF extends RunnerBuilder<SELF, CHANGE_ENTRY, CONFIG>,
+    CHANGE_ENTRY extends ChangeEntry,
+    CONFIG extends MongockConfiguration>
+    extends
+		ChangeLogScanner<SELF, CONFIG>,
+		MigrationWriter<SELF, CONFIG>,
+		LegacyMigrator<SELF, CONFIG>,
+		DriverConnectable<SELF, CHANGE_ENTRY, CONFIG>,
+		SystemVersionable<SELF, CONFIG>,
+		DependencyInjectable<SELF>,
+		ServiceIdentificable<SELF, CONFIG>,
+    MongockRunnable<SELF, CONFIG>,
+		Transactionable<SELF, CONFIG>,
+		TransactionStrategiable<SELF, CONFIG>,
+    Typeable<SELF, CONFIG> {
+}
---
+++ mongock-core/mongock-utils/src/main/java/io/mongock/utils/LogUtils.java
@@ -0,0 +1,31 @@
+package io.mongock.utils;
+
+import org.slf4j.Logger;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//TODO move to util module
+public final class LogUtils {
+
+  private LogUtils() {
+  }
+
+  public static void logMethodWithArguments(Logger logger, String methodName, List<Object> changelogInvocationParameters) {
+    String arguments = changelogInvocationParameters.stream()
+        .map(LogUtils::getParameterType)
+        .collect(Collectors.joining(", "));
+    logger.info("method[{}] with arguments: [{}]", methodName, arguments);
+
+  }
+
+  private static String getParameterType(Object obj) {
+    String className = obj != null ? obj.getClass().getName() : "{null argument}";
+    int mongockProxyPrefixIndex = className.indexOf(MongockProperties.PROXY_MONGOCK_PREFIX);
+    if(mongockProxyPrefixIndex > 0) {
+      return className.substring(0, mongockProxyPrefixIndex);
+    } else {
+      return className;
+    }
+  }
+}
