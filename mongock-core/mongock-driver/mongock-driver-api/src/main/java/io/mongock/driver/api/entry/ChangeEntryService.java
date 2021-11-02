1069c9206538e5543e9bc8407439c77c6d9c2562
dieppa <aperezdieppa@gmail.com>
2021-11-02 20:07:33 +0000

    pre-filter state

--- mongock-core/mongock-driver/mongock-driver-api/src/main/java/io/mongock/driver/api/entry/ChangeEntryService.java
+++ mongock-core/mongock-driver/mongock-driver-api/src/main/java/io/mongock/driver/api/entry/ChangeEntryService.java
@@ -8,9 +8,11 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import static io.mongock.driver.api.entry.ChangeState.EXECUTED;
+import static io.mongock.driver.api.entry.ChangeState.ROLLED_BACK;
 
 
 public interface ChangeEntryService<CHANGE_ENTRY extends ChangeEntry> extends RepositoryIndexable, Process {
@@ -33,14 +35,18 @@ public interface ChangeEntryService<CHANGE_ENTRY extends ChangeEntry> extends Re
    * @throws MongockException 
    */
   default List<ExecutedChangeEntry> getExecuted() throws MongockException {
+
+    Predicate<CHANGE_ENTRY> filterState = entry -> entry.getState() == null || entry.getState() == EXECUTED || entry.getState() == ROLLED_BACK;
+
     return getAll()
         .stream()
         .collect(Collectors.groupingBy(ChangeEntry::getChangeId))//Maps of List<ChangeEntry>, indexed by changeId
         .values()//collection of List<ChangeEntry>
         .stream()
         .peek(duplicatedEntries -> duplicatedEntries.sort((c1, c2) -> c2.getTimestamp().compareTo(c1.getTimestamp())))//sorts each list in the map by date in reverse
+        .map(duplicatedEntries -> duplicatedEntries.stream().filter(filterState).collect(Collectors.toList()))//only takes into account executed or rolled back
         .map(duplicatedEntries-> duplicatedEntries.get(0))//transform each list in a single ChangeEntry. The first one
-        .filter(entry -> EXECUTED == entry.getState())//only gets the ones that are executed
+        .filter(entry -> entry.getState() == null || entry.getState() == EXECUTED)//only gets the ones that are executed
         .sorted(Comparator.comparing(ChangeEntry::getTimestamp))// Sorts the resulting list chronologically
         .map(ExecutedChangeEntry::new)//transform the entry to an executed entry
         .collect(Collectors.toList());
---
+++ mongock-core/mongock-driver/mongock-driver-api/src/test/java/io/mongock/driver/api/entry/ChangeEntryServiceTest.java
@@ -0,0 +1,72 @@
+package io.mongock.driver.api.entry;
+
+import io.mongock.api.exception.MongockException;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static io.mongock.driver.api.entry.ChangeState.EXECUTED;
+import static org.junit.Assert.*;
+
+public class ChangeEntryServiceTest {
+
+  @Test
+  public void getExecuted() {
+  }
+
+
+  static class ChangeEntryServiceImpl implements ChangeEntryService<ChangeEntry> {
+
+
+    @Override
+    public List<ChangeEntry> getAll() {
+      Instant now = Instant.now();
+      return Arrays.asList(
+          getChangeEntry("changeEntry-1", now, EXECUTED)
+      );
+    }
+
+    static ChangeEntry getChangeEntry(String changeId, Instant instant, ChangeState state) {
+      return new ChangeEntry(
+          "executionId",
+          changeId,
+          "author",
+          Date.from(instant),
+          EXECUTED,
+          ChangeType.EXECUTION,
+          "changeLogClass",
+          "changeSetMethod",
+          1000L,
+          "executionHostname",
+          new Object());
+    }
+
+    @Override
+    public void setIndexCreation(boolean indexCreation) {
+
+    }
+
+    @Override
+    public boolean isAlreadyExecuted(String changeSetId, String author) throws MongockException {
+      return false;
+    }
+
+    @Override
+    public void saveOrUpdate(ChangeEntry changeEntry) throws MongockException {
+
+    }
+
+    @Override
+    public void save(ChangeEntry changeEntry) throws MongockException {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+  }
+}
