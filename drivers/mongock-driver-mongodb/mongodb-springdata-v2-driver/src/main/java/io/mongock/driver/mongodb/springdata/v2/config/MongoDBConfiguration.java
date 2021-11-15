1695eba7da6944677e20004d423527b7bfcbab15
Dieppa on the rocks <aperezdieppa@gmail.com>
2021-11-15 23:15:49 +0000

    removed @Configuration annotation (#421)

--- drivers/mongock-driver-mongodb/mongodb-springdata-v2-driver/src/main/java/io/mongock/driver/mongodb/springdata/v2/config/MongoDBConfiguration.java
+++ drivers/mongock-driver-mongodb/mongodb-springdata-v2-driver/src/main/java/io/mongock/driver/mongodb/springdata/v2/config/MongoDBConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.concurrent.TimeUnit;
 
-@Configuration
 @ConfigurationProperties("mongock.mongo-db")
 public class MongoDBConfiguration {
 
