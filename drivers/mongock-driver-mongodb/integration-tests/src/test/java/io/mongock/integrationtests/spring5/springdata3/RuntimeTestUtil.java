39ddb100a67874af29bba8611c4341b22cc152b3
Philippe Perrault <philippe.perrault@gmail.com>
2021-12-13 15:35:28 -0500

    fix for https://github.com/mongock/mongock/issues/455 (#456)
    Co-authored-by: Philippe <philippe.perrault@ericsson.com>

--- drivers/mongock-driver-mongodb/integration-tests/src/test/java/io/mongock/integrationtests/spring5/springdata3/RuntimeTestUtil.java
+++ drivers/mongock-driver-mongodb/integration-tests/src/test/java/io/mongock/integrationtests/spring5/springdata3/RuntimeTestUtil.java
@@ -1,6 +1,7 @@
 package io.mongock.integrationtests.spring5.springdata3;
 
 import io.mongock.integrationtests.spring5.springdata3.util.MongoContainer;
+import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.HashMap;
@@ -10,73 +11,94 @@ import java.util.stream.Collectors;
 
 public abstract class RuntimeTestUtil {
 
-    public static final String DEFAULT_DATABASE_NAME = "mongocktest";
+  public static final String DEFAULT_DATABASE_NAME = "mongocktest";
 
 
-    public static MongoContainer startMongoDbContainer(String mongoDockerImage) {
-        MongoContainer mongoContiner =  MongoContainer.builder().mongoDockerImageName(mongoDockerImage).build();
-        mongoContiner.start();
+  public static MongoContainer startMongoDbContainer(String mongoDockerImage) {
+    MongoContainer mongoContiner = MongoContainer.builder().mongoDockerImageName(mongoDockerImage).build();
+    mongoContiner.start();
 
-        String replicaSetUrl = mongoContiner.getReplicaSetUrl();
-        return mongoContiner;
+    String replicaSetUrl = mongoContiner.getReplicaSetUrl();
+    return mongoContiner;
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndDefaultPackage(String mongoVersion) {
+    return startSpringAppWithMongoDbVersionAndDefaultPackage(
+        mongoVersion,
+        false
+    );
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndDefaultPackage(String mongoVersion, boolean sleuthEnabled) {
+    return startSpringAppWithMongoDbVersionAndPackage(
+        mongoVersion,
+        "io.mongock.integrationtests.spring5.springdata3.changelogs.client",
+        sleuthEnabled
+    );
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndNoPackage(String mongoDBVersion, boolean sleuthEnabled) {
+    Map<String, String> parameters = new HashMap<>();
+    parameters.put("spring.sleuth.enabled", String.valueOf(sleuthEnabled));
+    parameters.put("mongock.changeLogsScanPackage", "");
+    parameters.put("mongock.transactionable", "false");
+    return startSpringAppWithMongoDbVersionAndParameters(mongoDBVersion, parameters);
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndNoPackage(String mongoDBVersion) {
+    return startSpringAppWithMongoDbVersionAndNoPackage(mongoDBVersion, false);
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndPackage(String mongoDBVersion, String packagePath) {
+    return startSpringAppWithMongoDbVersionAndPackage(mongoDBVersion, packagePath, false);
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndPackage(String mongoDBVersion, String packagePath, boolean sleuthEnabled) {
+    Map<String, String> parameters = new HashMap<>();
+    parameters.put("spring.sleuth.enabled", String.valueOf(sleuthEnabled));
+    parameters.put("mongock.changeLogsScanPackage", packagePath);
+    return startSpringAppWithMongoDbVersionAndParameters(mongoDBVersion, parameters);
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithTransactionDisabledMongoDbVersionAndPackage(String mongoDBVersion, String packagePath, boolean sleuthEnabled) {
+    Map<String, String> parameters = new HashMap<>();
+    parameters.put("spring.sleuth.enabled", String.valueOf(sleuthEnabled));
+    parameters.put("mongock.changeLogsScanPackage", packagePath);
+    parameters.put("mongock.transactionable", "false");
+    return startSpringAppWithMongoDbVersionAndParameters(mongoDBVersion, parameters);
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithTransactionDisabledMongoDbVersionAndPackage(String mongoDBVersion, String packagePath) {
+    return startSpringAppWithTransactionDisabledMongoDbVersionAndPackage(mongoDBVersion, packagePath, false);
+  }
+
+  public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndParameters(String mongoDBVersion, Map<String, String> parameters) {
+    return startSpringAppWithParameters(startMongoDbContainer(mongoDBVersion), parameters);
+  }
+
+
+  public static ConfigurableApplicationContext startSpringAppWithParameters(MongoContainer container, Map<String, String> parameters) {
+    String[] parametersArray = getParametersArray(container, parameters);
+    return Mongock4Spring5SpringData3App.getSpringAppBuilder().properties(parametersArray).run();
+  }
+
+  private static String[] getParametersArray(MongoContainer container, Map<String, String> parameters) {
+    String replicaSetUrl = container.getReplicaSetUrl();
+    List<String> parametersList = parameters.entrySet()
+        .stream()
+        .map(entry -> String.format("%s=%s", entry.getKey(), entry.getValue()))
+        .collect(Collectors.toList());
+    if (!parameters.containsKey("server.port")) {
+      parametersList.add("server.port=0");
     }
-
-    public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndDefaultPackage(String mongoVersion) {
-        return startSpringAppWithMongoDbVersionAndPackage(
-                mongoVersion,
-                "io.mongock.integrationtests.spring5.springdata3.changelogs.client"
-        );
+    if (!parameters.containsKey("mongock.transactionable")) {
+      parametersList.add("mongock.transactionable=" + container.isTransactionable());
     }
-
-    public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndNoPackage(String mongoDBVersion) {
-
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("mongock.changeLogsScanPackage", "");
-        parameters.put("mongock.transactionable", "false");
-        return startSpringAppWithMongoDbVersionAndParameters(mongoDBVersion, parameters);
-    }
-
-    public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndPackage(String mongoDBVersion, String packagePath) {
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("mongock.changeLogsScanPackage", packagePath);
-        return startSpringAppWithMongoDbVersionAndParameters(mongoDBVersion, parameters);
-    }
-
-    public static ConfigurableApplicationContext startSpringAppWithTransactionDisabledMongoDbVersionAndPackage(String mongoDBVersion, String packagePath) {
-        Map<String, String> parameters = new HashMap<>();
-        parameters.put("mongock.changeLogsScanPackage", packagePath);
-        parameters.put("mongock.transactionable", "false");
-        return startSpringAppWithMongoDbVersionAndParameters(mongoDBVersion, parameters);
-    }
-
-
-    public static ConfigurableApplicationContext startSpringAppWithMongoDbVersionAndParameters(String mongoDBVersion, Map<String, String> parameters) {
-        return startSpringAppWithParameters(startMongoDbContainer(mongoDBVersion), parameters);
-    }
-
-
-    public static ConfigurableApplicationContext startSpringAppWithParameters(MongoContainer container, Map<String, String> parameters) {
-        String[] parametersArray = getParametersArray(container, parameters);
-        return Mongock4Spring5SpringData3App.getSpringAppBuilder().properties(parametersArray).run();
-    }
-
-    private static String[] getParametersArray(MongoContainer container, Map<String, String> parameters) {
-        String replicaSetUrl = container.getReplicaSetUrl();
-        List<String> parametersList = parameters.entrySet()
-                .stream()
-                .map(entry -> String.format("%s=%s", entry.getKey(), entry.getValue()))
-                .collect(Collectors.toList());
-        if(!parameters.containsKey("server.port")) {
-            parametersList.add("server.port=0");
-        }
-        if(!parameters.containsKey("mongock.transactionable")) {
-            parametersList.add("mongock.transactionable=" + container.isTransactionable());
-        }
-        parametersList.add("spring.data.mongodb.uri=" + replicaSetUrl);
-        parametersList.add("spring.data.mongodb.database=" + DEFAULT_DATABASE_NAME);
-        String[] parametersArray = new String[parametersList.size()];
-        return parametersList.toArray(parametersArray);
-    }
+    parametersList.add("spring.data.mongodb.uri=" + replicaSetUrl);
+    parametersList.add("spring.data.mongodb.database=" + DEFAULT_DATABASE_NAME);
+    String[] parametersArray = new String[parametersList.size()];
+    return parametersList.toArray(parametersArray);
+  }
 
 
 }
--- drivers/mongock-driver-mongodb/mongodb-springdata-v3-driver/src/main/java/io/mongock/driver/mongodb/springdata/v3/config/SpringDataMongoV3ContextBase.java
+++ drivers/mongock-driver-mongodb/mongodb-springdata-v3-driver/src/main/java/io/mongock/driver/mongodb/springdata/v3/config/SpringDataMongoV3ContextBase.java
@@ -1,14 +1,14 @@
 package io.mongock.driver.mongodb.springdata.v3.config;
 
+import com.mongodb.ReadConcern;
 import io.mongock.api.config.MongockConfiguration;
 import io.mongock.driver.api.driver.ConnectionDriver;
 import io.mongock.driver.mongodb.springdata.v3.SpringDataMongoV3DriverBase;
-import com.mongodb.ReadConcern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.Optional;
 
@@ -19,7 +19,7 @@ public abstract class SpringDataMongoV3ContextBase<CONFIG extends MongockConfigu
   public ConnectionDriver connectionDriver(MongoTemplate mongoTemplate,
                                                          CONFIG config,
                                                          MongoDBConfiguration mongoDbConfig,
-                                                         Optional<MongoTransactionManager> txManagerOpt) {
+                                                         Optional<PlatformTransactionManager> txManagerOpt) {
     DRIVER driver = buildDriver(mongoTemplate, config, mongoDbConfig, txManagerOpt);
     setGenericDriverConfig(config, txManagerOpt, driver);
     setMongoDBConfig(mongoDbConfig, driver);
@@ -30,10 +30,10 @@ public abstract class SpringDataMongoV3ContextBase<CONFIG extends MongockConfigu
   protected abstract DRIVER buildDriver(MongoTemplate mongoTemplate,
                                         CONFIG config,
                                         MongoDBConfiguration mongoDbConfig,
-                                        Optional<MongoTransactionManager> txManagerOpt);
+                                        Optional<PlatformTransactionManager> txManagerOpt);
 
   private void setGenericDriverConfig(CONFIG config,
-                                      Optional<MongoTransactionManager> txManagerOpt,
+                                      Optional<PlatformTransactionManager> txManagerOpt,
                                       DRIVER driver) {
     txManagerOpt.ifPresent(driver::enableTransactionWithTxManager);
     driver.setChangeLogRepositoryName(config.getChangeLogRepositoryName());
