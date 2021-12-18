7ff09942357c295726c6d6d2de9f6cb60643b0d4
Dieppa on the rocks <aperezdieppa@gmail.com>
2021-12-18 14:56:25 +0000

    using PlatformTransactionManager in SperingData v2 (#470)

--- drivers/mongock-driver-mongodb/mongodb-springdata-v2-driver/src/main/java/io/mongock/driver/mongodb/springdata/v2/config/SpringDataMongoV2Context.java
+++ drivers/mongock-driver-mongodb/mongodb-springdata-v2-driver/src/main/java/io/mongock/driver/mongodb/springdata/v2/config/SpringDataMongoV2Context.java
@@ -2,14 +2,14 @@ package io.mongock.driver.mongodb.springdata.v2.config;
 
 import io.mongock.api.config.MongockConfiguration;
 import io.mongock.driver.mongodb.springdata.v2.SpringDataMongoV2Driver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import java.util.Optional;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 
 @Configuration
 @ConditionalOnExpression("${mongock.enabled:true}")
@@ -21,7 +21,7 @@ public class SpringDataMongoV2Context extends SpringDataMongoV2ContextBase<Mongo
   protected SpringDataMongoV2Driver buildDriver(MongoTemplate mongoTemplate,
                                               MongockConfiguration config,
                                               MongoDBConfiguration mongoDbConfig,
-                                              Optional<MongoTransactionManager> txManagerOpt) {
+                                              Optional<PlatformTransactionManager> txManagerOpt) {
     return SpringDataMongoV2Driver.withLockStrategy(
         mongoTemplate,
         config.getLockAcquiredForMillis(),
