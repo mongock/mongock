319f69d3b38c36e18b68eb4ce88d1da7b82452d4
Oliver Santana <oliver.sm@gmail.com>
2022-03-10 11:39:01 +0000

    WIP

--- mongock-core/mongock-runner/mongock-runner-core/src/main/java/io/mongock/runner/core/executor/ExecutorBuilderDefault.java
+++ mongock-core/mongock-runner/mongock-runner-core/src/main/java/io/mongock/runner/core/executor/ExecutorBuilderDefault.java
@@ -22,6 +22,16 @@ public class ExecutorBuilderDefault extends ExecutorBuilderBase<MongockConfigura
         throw new MongockException(String.format("Operation '%s' not found. It may be a professional operation and the professional library is not provided ", operation.getId()));
     }
   }
+  
+  public <CONFIG extends MongockConfiguration, EXECUTOR extends ExecutorBuilderBase<CONFIG>> ExecutorBuilderDefault initializeFrom(EXECUTOR from) {
+    this.operation = from.operation;
+    this.executionId = from.executionId;
+    this.driver = from.driver;
+    this.config = from.config;
+    this.changeLogService = from.changeLogService;
+    this.changeLogRuntime = from.changeLogRuntime;
+    return this;
+  }
 }
 
 
