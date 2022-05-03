f6ccd4e065a31ed7c4425b24055a9e721e8be0a7
antoniop perez dieppa <aperezdieppa@gmail.com>
2022-05-03 11:33:00 +0100

    removed mongock-standalone-junit5

--- mongock-test/mongock-standalone-test/src/main/java/io/mongock/test/standalone/MongockStandaloneIntegrationTestBase.java
+++ mongock-test/mongock-standalone-test/src/main/java/io/mongock/test/standalone/MongockStandaloneIntegrationTestBase.java
@@ -1,15 +1,8 @@
 package io.mongock.test.standalone;
 
-import io.mongock.runner.core.builder.RunnerBuilder;
 import io.mongock.test.base.MongockIntegrationTestBase;
 
 public class MongockStandaloneIntegrationTestBase extends MongockIntegrationTestBase {
-  protected MongockStandaloneIntegrationTestBase() {
-    super();
-  }
 
-  protected MongockStandaloneIntegrationTestBase(RunnerBuilder builder) {
-    super(builder);
-  }
 
 }
