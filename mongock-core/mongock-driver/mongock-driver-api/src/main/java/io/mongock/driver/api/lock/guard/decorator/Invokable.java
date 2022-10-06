e729567a749b65498977a2dc26562bf3dfbfefff
dieppa <aperezdieppa@gmail.com>
2022-10-06 07:44:41 +0100

    removed decorator module

--- mongock-core/mongock-driver/mongock-driver-api/src/main/java/io/mongock/driver/api/lock/guard/decorator/Invokable.java
+++
@@ -1,7 +0,0 @@
-package io.mongock.driver.api.lock.guard.decorator;
-
-import io.mongock.driver.api.lock.guard.invoker.LockGuardInvoker;
-
-public interface Invokable {
-  LockGuardInvoker getInvoker();
-}
