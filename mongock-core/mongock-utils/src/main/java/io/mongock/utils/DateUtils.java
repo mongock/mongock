dd9cc34fbcde491fe636f0383ceb1485aad8345e
SerkanKaya <kayaserkan@gmail.com>
2024-04-16 18:29:46 +0300

    OffsetDateTime support on DateUtils class (#668)
    Issue reference
    Fixes #667
    
    Description and context
    OffsetDateTime support is added
    
    Benefits
    Makes Mongock working flawlessly wtih projects using OffsetDateTime format.
    
    Possible Drawbacks
    No drawback is expected
    
    Co-authored-by: SerkanKaya <129129skaya129129@gmail.com>

--- mongock-core/mongock-utils/src/main/java/io/mongock/utils/DateUtils.java
+++ mongock-core/mongock-utils/src/main/java/io/mongock/utils/DateUtils.java
@@ -1,6 +1,7 @@
 package io.mongock.utils;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
@@ -19,14 +20,22 @@ public final class DateUtils {
     else if (value.getClass().equals(LocalDateTime.class)) {
       return localDateTimeToDate((LocalDateTime)value);
     }
+    else if (value.getClass().equals(OffsetDateTime.class)) {
+      return offsetDateTimeToDate((OffsetDateTime)value);
+    }
     else {
       throw new RuntimeException(String.format("%s cannot be cast to %s", value.getClass().getName(), Date.class.getName()));
     }
   }
-  
+
   private static Date localDateTimeToDate(LocalDateTime dateToConvert) {
-      return Date
-        .from(dateToConvert.atZone(ZoneId.systemDefault())
-        .toInstant());
+    return Date
+            .from(dateToConvert.atZone(ZoneId.systemDefault())
+                    .toInstant());
   }
+
+  private static Date offsetDateTimeToDate(OffsetDateTime dateToConvert) {
+    return Date.from(dateToConvert.toInstant());
+  }
+
 }
