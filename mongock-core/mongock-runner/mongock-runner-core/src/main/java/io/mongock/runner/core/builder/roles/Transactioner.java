ad2a1e3cba4fc0f329ebccdab09eff54d0173b9f
Oliver Santana <oliver.sm@gmail.com>
2024-08-10 00:39:12 +0100

    issue/#676 New "transactional" property in favour of "transactionEnabled". Added property setter to RunnerBuilder.

--- mongock-core/mongock-runner/mongock-runner-core/src/main/java/io/mongock/runner/core/builder/roles/Transactioner.java
+++ mongock-core/mongock-runner/mongock-runner-core/src/main/java/io/mongock/runner/core/builder/roles/Transactioner.java
@@ -7,13 +7,27 @@ import io.mongock.api.config.MongockConfiguration;
 public interface Transactioner<SELF extends Transactioner<SELF, CONFIG>, CONFIG extends MongockConfiguration>
     extends Configurable<SELF, CONFIG>, SelfInstanstiator<SELF> {
   /**
+   * @deprecated  As of release 5.5, replaced by {@link #setTransactional}
+   * 
    * Indicates if Mongock should run in transaction mode or not
    *
    * @param transactionEnabled if Mongock should run in transaction mode
    * @return builder for fluent interface
    */
+  @Deprecated
   default SELF setTransactionEnabled(boolean transactionEnabled) {
     getConfig().setTransactionEnabled(transactionEnabled);
+    return getInstance();
+  }
+  
+  /**
+   * Indicates if Mongock should run in transaction mode or not
+   *
+   * @param transactional if Mongock should run in transaction mode
+   * @return builder for fluent interface
+   */
+  default SELF setTransactional(boolean transactional) {
+    getConfig().setTransactional(transactional);
     return getInstance();
   }
 }
